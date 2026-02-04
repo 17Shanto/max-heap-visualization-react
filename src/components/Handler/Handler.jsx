19b71fe9711d128bc74e820aa5d68170486436f9
17Shanto <shahporanshanto23@gmail.com>
2026-02-05 02:58:28 +0600

    feat:implement reset heap data function

--- src/components/Handler/Handler.jsx
+++ src/components/Handler/Handler.jsx
@@ -1,17 +1,32 @@
 import React, { useRef, useState } from "react";
 import { useGraphData } from "../../context/DataContext";
+import data from "../../../public/data";
 
 const Handler = () => {
   const [id, setId] = useState(31);
   const { graphData, updateGraphData } = useGraphData();
+  const [isResetting, setIsResetting] = useState(false);
   const weightInputRef = useRef(null);
+
   const handleSubmit = () => {
     setId((prevId) => prevId + 1);
     const value = weightInputRef.current.value;
+    if (!value) return;
     const weightValue = parseFloat(value);
     const newData = [...graphData, { personId: id, weight: weightValue }];
     updateGraphData(newData);
+    weightInputRef.current.value = "";
   };
+
+  const handleReset = () => {
+    setIsResetting(true);
+    setTimeout(() => {
+      const newData = [...data];
+      updateGraphData(newData);
+      setIsResetting(false);
+    }, 500);
+  };
+
   return (
     <div>
       <div className="">
@@ -31,9 +46,15 @@ const Handler = () => {
         </div>
 
         <div className="mt-4">
-          <button className="btn btn-info">
-            <span className="loading loading-spinner"></span>
-            Reset
+          <button
+            onClick={handleReset}
+            className="btn btn-outline"
+            disabled={isResetting}
+          >
+            {isResetting && (
+              <span className="loading loading-infinity text-2xl text-info"></span>
+            )}
+            Reset Data
           </button>
         </div>
       </div>
