48b637edd30524d8fbd5b70f84e2ec087667484a
17Shanto <shahporanshanto23@gmail.com>
2026-02-06 15:19:40 +0600

    feat: add dark and light theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TreeGraph from "./components/Tree/TreeGraph";
 import Handler from "./components/Handler/Handler";
 import { DataProvider } from "./context/DataContext";
 import InputStack from "./components/InputStack/InputStack";
+import { FaSun, FaMoon } from "react-icons/fa";
 
 const App = () => {
+  const [theme, setTheme] = useState("light");
+  useEffect(() => {
+    document.querySelector("html").setAttribute("data-theme", theme);
+  }, [theme]);
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-slate-50">
-      <div className="w-full bg-white  shadow-xl border border-slate-200 overflow-hidden">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-base-200 transition-colors duration-300">
+      <div className="w-full bg-base-100 shadow-xl border border-base-300 overflow-hidden">
         <div className="px-6 py-4 bg-slate-900 flex justify-between items-center">
           <h2 className="text-white font-semibold text-lg flex items-center gap-2">
             <span className="h-3 w-3 bg-green-400 rounded-full animate-pulse"></span>
@@ -24,6 +33,20 @@ const App = () => {
           <div>
             <div className="grid grid-cols-1 lg:grid-cols-4 gap-10">
               <div className="lg:col-span-1 p-4">
+                <div className="flex justify-between items-center mb-4 bg-base-200 p-3 rounded-lg">
+                  <span className="text-xl font-semibold opacity-70">
+                    Theme
+                  </span>
+                  <label className="swap swap-rotate">
+                    <input
+                      type="checkbox"
+                      onChange={toggleTheme}
+                      checked={theme === "dark"}
+                    />
+                    <FaSun className="swap-on fill-current w-6 h-6 text-yellow-500" />
+                    <FaMoon className="swap-off fill-current w-6 h-6 text-blue-500" />
+                  </label>
+                </div>
                 <Handler></Handler>
               </div>
               <div className="lg:col-span-3">
@@ -37,7 +60,7 @@ const App = () => {
             </div>
           </div>
         </DataProvider>
-        <div className="px-6 py-3 bg-slate-50 border-t border-slate-100 flex justify-between text-xs text-slate-400">
+        <div className="px-6 py-3 bg-base-200 border-t border-base-300 flex justify-between text-xs text-base-content/60">
           <span>Scale: 100%</span>
           <span>Status: Live Rendering</span>
         </div>
